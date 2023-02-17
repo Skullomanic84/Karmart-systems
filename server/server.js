ff7c988fbb9b9524586b6843028732d0703e18fa
Jahmaine Shiloh <55574810+Skullomanic84@users.noreply.github.com>
2023-02-17 17:42:21 +0200

    updated server.js
    1.added the register router path.
    2.added mongoose.set to remove deprecated error notice(bug)

--- server/server.js
+++ server/server.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = ('body-parser');
+const bodyParser = require ('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
+const userRoute = require('./routes/userRoute');
 
-
+const app = express();
 
 //middleware
-const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
+
+
+
+//routes middeleware
+app.use("/api/v1/users", userRoute);
 
 
 //routes
@@ -18,6 +25,7 @@ app.use(express.json());
 
 
 // MONGOOSE SET UP
+mongoose.set("strictQuery", false);
 const PORT = process.env.PORT || 6001;
 //connect to DB and start server
 mongoose
