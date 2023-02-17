3f8c8e4bb44d1b099109f4e7b5bc0bfb946fafe6
Jahmaine Shiloh <55574810+Skullomanic84@users.noreply.github.com>
2023-02-17 19:59:16 +0200

    Updated userModel.js
    added bcrypt plugin to hash the password. It's to allow reusable password hashing to include encrypting password, and password reset without having 3 different encryptions

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,32 +1,46 @@
-const mongoose = require('mongoose');
+//import 3rd party libraries
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Fill in your name"],
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Fill in your name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Fill in your name"],
+      unique: true,
+      trim: true,
+      match: [
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+        "Please enter a valid email address",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+      minLength: [8, "Password must be up to 8 characters"],
+    },
   },
-  email: {
-    type: String,
-    required: [true, "Fill in your name"],
-    unique: true,
-    trim: true,
-    match: [
-      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-      "Please enter a valid email address",
-    ],
-  },
-  password: {
-    type: String,
-    required: [true, "Please add a password"],
-    minLength: [6, "Password must be up to 6 characters"],
-    //   maxLength: [23, "Password must not be more than 23 characters"],
-  },
-},
-{
+  {
     timestamps: true,
   }
 );
+//password encryption(salt)
+userSchema.pre("save", async function (next) {
+  //when a password reset is done
+  if (!this.isModified("password")) {
+    return next();
+  }
+  //hashed password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(this.password, salt);
+  this.password = hashedPassword;
+  next();
+});
 
-const User = mongoose.model("User", userSchema)
+const User = mongoose.model("User", userSchema);
 
-module.exports = User
+module.exports = User;
