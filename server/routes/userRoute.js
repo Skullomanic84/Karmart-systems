ed31ac7b87900e0626fca96a1a61a2676dc17c15
Jahmaine Shiloh <55574810+Skullomanic84@users.noreply.github.com>
2023-02-18 01:22:39 +0200

    Update userRoute.js
    imported
    registerUser,
    loginUser,
    logout,
    getUser,
    loginStatus,
    updateUser,
    changePassword,
    forgotPassword,
    resetPassword,
    
    and set-up the appropriate route path

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,9 +1,32 @@
+//import from 3rd party library
 const express = require("express");
-const { registerUser } = require("../controllers/userController");
 const router = express.Router();
 
+//import from local files
+const {
+  registerUser,
+  loginUser,
+  logout,
+  getUser,
+  loginStatus,
+  updateUser,
+  changePassword,
+  forgotPassword,
+  resetPassword,
+} = require("../controllers/userController");
+const protect = require("../middleWare/authMiddleware");
 
-//registerUser function to fire whenever register endpoint is requested
+
+
+//function to fire whenever rendpoints are requested
 router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/logout", logout);
+router.get("/getuser", protect, getUser);
+router.get("/loggedin", loginStatus);
+router.patch("/updateuser", protect, updateUser);
+router.patch("/changepassword", protect, changePassword);
+router.post("/forgotpassword", forgotPassword);
+router.put("/resetpassword/:resetToken", resetPassword);
 
 module.exports = router;
