0e1eec8ef4e2c613df968bb8300c97d22f032692
Jahmaine Shiloh <55574810+Skullomanic84@users.noreply.github.com>
2023-02-22 14:46:18 +0200

    update userModel.js
    updated import User path

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,46 +1,17 @@
-//import 3rd party libraries
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
 
-const userSchema = mongoose.Schema(
+const UserSchema = new mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: [true, "Fill in your name"],
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: {
+      type: Boolean,
+      default: false,
     },
-    email: {
-      type: String,
-      required: [true, "Fill in your name"],
-      unique: true,
-      trim: true,
-      match: [
-        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-        "Please enter a valid email address",
-      ],
-    },
-    password: {
-      type: String,
-      required: [true, "Please add a password"],
-      minLength: [8, "Password must be up to 8 characters"],
-    },
+    img: { type: String },
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
-//password encryption(salt)
-userSchema.pre("save", async function (next) {
-  //when a password reset is done
-  if (!this.isModified("password")) {
-    return next();
-  }
-  //hashed password
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(this.password, salt);
-  this.password = hashedPassword;
-  next();
-});
 
-const User = mongoose.model("User", userSchema);
-
-module.exports = User;
+module.exports = mongoose.model("User", UserSchema);
